fa1c52080031b1743e0227e6515f1043ae9d1fa9
branchpanic <branchpanic@gmail.com>
2021-08-18 18:07:11 -0400

    Exploration start

---
+++ README.md
@@ -0,0 +1,14 @@
+# vdcrpt Experimental
+
+This is an experimental branch of vdcrpt, rewritten in C#. Nothing is
+guaranteed to come of it, but it may serve as the basis for a more powerful
+tool.
+
+## Subprojects
+
+Vdcrpt.Avi is a binary AVI manipulation library modeled after
+[ucnv/aviglitch].
+
+Vdcrpt.UI is the actual GUI application.
+
+[ucnv/aviglitch]: https://github.com/ucnv/aviglitch/blob/master/lib/aviglitch/frames.rb
---
+++ Vdcrpt.Avi/AviFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Vdcrpt.Avi
+{
+    // https://github.com/ucnv/aviglitch/blob/master/lib/aviglitch/frames.rb
+    // https://docs.microsoft.com/en-us/windows/win32/directshow/avi-riff-file-reference
+    
+    public class AviFile
+    {
+        public AviFile(string path)
+        {
+            using var stream = new FileStream(path, FileMode.Open);
+            using var reader = new BinaryReader(stream);
+
+            throw new NotImplementedException();
+        }
+    }
+}
