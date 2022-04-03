dce50f8ae38d3f88782e7e62c7902321e8e77560
branchpanic <branchpanic@gmail.com>
2022-04-03 12:36:40 -0700

    Set up macOS deployment

--- tasks.py
+++ tasks.py
@@ -21,6 +21,23 @@ FFMPEG_BUILDS = {
 }
 
 
+DIST_DIR = Path('./dist')
+TEMP_DIR = Path('./temp')
+
+
+def _get_system_runtime():
+    platform = sys.platform
+
+    if platform == 'win32':
+        return 'win-x64'
+    elif platform == 'darwin':
+        return 'osx-x64'
+    elif platform == 'linux':
+        return 'linux-x64'
+
+    return ''
+
+
 def _ensure_dir():
     if not Path("./Vdcrpt.sln").is_file():
         print("This script must be run from the root of the project.")
@@ -32,15 +49,16 @@ def clean(c):
     _ensure_dir()
     c.run("dotnet clean")
 
-    for script_dir in ("temp", "dist"):
-        path = Path(script_dir)
+    for script_dir in (DIST_DIR, TEMP_DIR):
         path.rmdir()
         path.mkdir(exist_ok=True)
 
 
 @task
-def fetch_ffmpeg(c, runtime="win-x64"):
+def fetch_ffmpeg(c, runtime=""):
     _ensure_dir()
+
+    runtime = runtime or _get_system_runtime()
 
     try:
         (url, ext) = FFMPEG_BUILDS[runtime]
@@ -48,6 +66,7 @@ def fetch_ffmpeg(c, runtime="win-x64"):
         print("Unsupported runtime: {}".format(runtime))
         sys.exit(1)
 
+    Path('temp').mkdir(exist_ok=True)
     archive_path = Path(f"temp/ffmpeg-{runtime}").with_suffix(ext)
     binary_path = Path(f"temp/ffmpeg-{runtime}")
     if runtime == "win-x64":
@@ -65,19 +84,19 @@ def fetch_ffmpeg(c, runtime="win-x64"):
     elif runtime == "osx-x64":
         c.run(f"7z e {archive_path} -otemp ffmpeg -r -y")
         Path("temp/ffmpeg").rename(binary_path)
-        # xattr -r -d com.apple.quarantine <something>
-        raise NotImplementedError(runtime)
     elif runtime == "linux-x64":
         raise NotImplementedError(runtime)
 
 
 @task(fetch_ffmpeg)
-def dist(c, runtime="win-x64"):
+def dist(c, runtime=""):
     _ensure_dir()
+
+    runtime = runtime or _get_system_runtime()
 
     try:
         tree = ET.parse("./src/Vdcrpt.Desktop/Vdcrpt.Desktop.csproj")
-        version = tree.findtext(".//AssemblyVersion")
+        version = tree.findtext(".//Version")
     except:
         print("Warning: could not determine version", file=sys.stderr)
         version = "UNKNOWN"
@@ -98,11 +117,13 @@ def dist(c, runtime="win-x64"):
         )
 
         # Copy resources
-        shutil.copy(f"./temp/ffmpeg-win-x64.exe", f"dist/vdcrpt-win-x64/ffmpeg.exe")
+        shutil.copy(f"./temp/ffmpeg-win-x64.exe",
+                    f"dist/vdcrpt-win-x64/ffmpeg.exe")
         shutil.copy(f"./LICENSE", f"dist/vdcrpt-win-x64/LICENSE.txt")
         shutil.copy(f"./README_USER.md", f"dist/vdcrpt-win-x64/README.txt")
 
-        shutil.make_archive(f"dist/vdcrpt-win-x64", "zip", f"dist/vdcrpt-win-x64")
+        shutil.make_archive(f"dist/vdcrpt-win-x64",
+                            "zip", f"dist/vdcrpt-win-x64")
 
     elif runtime == "linux-x64":
         # c.run(f'dotnet clean')
@@ -111,12 +132,37 @@ def dist(c, runtime="win-x64"):
         raise NotImplementedError(runtime)
 
     elif runtime == "osx-x64":
+        shutil.rmtree(f"dist/vdcrpt-osx-x64", ignore_errors=True)
         if sys.platform != "darwin":
             print(f"Builds for {runtime} are only supported on macOS.")
             sys.exit(1)
 
         # Use dotnet-bundle to generate an app bundle on macOS
-        raise NotImplementedError(runtime)
+        c.run("dotnet restore ./src/Vdcrpt.Desktop -r osx-x64")
+
+        dist_abs_path = DIST_DIR / 'vdcrpt-osx-x64'
+        dist_abs_path.mkdir(parents=True, exist_ok=True)
+
+        c.run(
+            f"dotnet msbuild ./src/Vdcrpt.Desktop -t:BundleApp -p:RuntimeIdentifier=osx-x64 -p:UseAppHost=true -p:Configuration=Release")
+        shutil.copytree(
+            "./src/Vdcrpt.Desktop/bin/Release/net5.0/osx-x64/publish/vdcrpt.app", dist_abs_path / "vdcrpt.app")
+        shutil.copy(f"./temp/ffmpeg-osx-x64",
+                    f"dist/vdcrpt-osx-x64/vdcrpt.app/Contents/MacOS/ffmpeg")
+        shutil.copy(f"./LICENSE", f"dist/vdcrpt-osx-x64/LICENSE.txt")
+        shutil.copy(f"./resources/vdcrpt.icns",
+                    f"dist/vdcrpt-osx-x64/vdcrpt.app/Contents/Resources/vdcrpt.icns")
+        shutil.copy(f"./README_USER.md", f"dist/vdcrpt-osx-x64/README.txt")
+
+        # Some kind of signature thing, not sure if this is right but Apple
+        # won't let us run otherwise and I don't want to buy a developer
+        # license. Copied from alacritty:
+        # https://github.com/alacritty/alacritty/blob/49d64fbeecbdde2293ca0e7c7346941791685c3e/Makefile#L55
+        c.run('codesign --remove-signature dist/vdcrpt-osx-x64/vdcrpt.app')
+        c.run('codesign --force --deep --sign - dist/vdcrpt-osx-x64/vdcrpt.app')
+
+        shutil.make_archive(f"dist/vdcrpt-osx-x64",
+                            "zip", f"dist/vdcrpt-osx-x64")
 
     else:
         print(f"Unsupported runtime: {runtime}")
